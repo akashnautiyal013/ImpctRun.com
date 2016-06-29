b31f1664cc78905bbc95b9b8372e9d606a05860f
root <akash.nautiyal013@gmail.com>
2016-06-29 04:15:55 -0400

    SeverLivePush

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -7,7 +7,23 @@ function HomeController($scope) {
             $("body").fadeIn(400);
         });
     };
-    new WOW().init();
+    var wow = new WOW(
+      {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       200,          // distance to the element when triggering the animation (default is 0)
+        mobile:       false,       // trigger animations on mobile devices (default is true)
+        live:         true,       // act on asynchronously loaded content (default is true)
+        callback:     function(box) {
+          // the callback is fired every time an animation is started
+          // the argument that is passed in is the DOM node being animated
+        },
+        scrollContainer: null // optional scroll container selector, otherwise use window
+      }
+    );
+    wow.init();
+    wow.sync();
+
       var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         paginationClickable: true,
