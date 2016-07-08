4f6e7d4ebedbf50e18ca7a8168ad5bd400768673
akashnautiyal013 <akash.nautiyal013@gmail.com>
2016-07-08 21:30:09 +0530

    SomeChangesOnMobWeb

--- js/app.js
+++ js/app.js
@@ -58,10 +58,10 @@
 
     config.$inject = ['$routeProvider', '$locationProvider',];
     function config($routeProvider, $locationProvider) {
-  //      $locationProvider.html5Mode({
-  //    enabled: true,
-  //      requireBase: false
-  // });
+       $locationProvider.html5Mode({
+     enabled: true,
+       requireBase: false
+  });
         $routeProvider
             .when('/', {
                 controller: 'HomeController',
