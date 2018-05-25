6f93898e2208285b8896053cdca0227856de0649
akashnautiyal013 <akash.nautiyal013@gmail.com>
2018-05-25 17:42:49 +0530

    signup api and signup mail2

--- Desktop/impact-node-master/controllers/user/socialLogin.js
+++ Desktop/impact-node-master/controllers/user/socialLogin.js
@@ -18,9 +18,9 @@ module.exports = {
     creatusersToken_token(req,res,next,provider,userResponse,token){
         moment.suppressDeprecationWarnings = true;
         console.log('userResponse')
-        var nowDate  =   moment().add(90, 'days').calendar();
-        var expireDate = moment(nowDate).format(); ;
-        let accessTokendata = {
+        var nowDate         =   moment().add(90, 'days').calendar();
+        var expireDate      =   moment(nowDate).format(); ;
+        let accessTokendata =  {
             token           :  token,
             expires         :  expireDate,
             scope           :  "read write",
@@ -31,6 +31,8 @@ module.exports = {
         db.usersToken.create(accessTokendata).then((accessTokendataresponse)=>{
             let usertoken = JSON.parse(JSON.stringify(accessTokendataresponse)).token
             let provider  = true;
+            var templet   =  "<div><h1>Welcome to Impact Club "+userResponse.first_name+" "+userResponse.last_name + " </h1><p>Get fit do good</p><img src='https://i.gifer.com/47in.gif' width='300' height='200'></div>"
+            sendmail.sendMail("Welcome "+userResponse.first_name,userResponse.email,"Hello","Welcome to Impact Club",templet);
             req.headers.authorization = 'bearer '+ usertoken;
             next();
              
@@ -69,23 +71,22 @@ module.exports = {
 
     // calling Facebook graph api for singning user and creating new user share_api_user table in db 
     authenticatingUserFacebook(token,req,res,next){
-        const tokenheader = req.headers.authorization;
-        var parts = tokenheader.split(' ');
-        const userFieldSet = 'id, name, about, email,gender,first_name,last_name,birthday,picture';
-        const options = {
-            method : 'GET',
-            uri    : `https://graph.facebook.com/me/`,
-            qs     : {
-                       access_token :  token,
-                       fields       :  userFieldSet
-                     }
-        };
+        const tokenheader   =  req.headers.authorization;
+        var parts           =  tokenheader.split(' ');
+        const userFieldSet  =  'id, name, about, email,gender,first_name,last_name,birthday,picture';
+        const options       =  {
+                                method : 'GET',
+                                uri    : `https://graph.facebook.com/me/`,
+                                qs     : {
+                                           access_token :  token,
+                                           fields       :  userFieldSet
+                                         }
+                                };
         request(options)
         .then(fbRes => {
             var tokengen = randomToken(25);
-            console.log('infacebook',tokengen);
             let userdata = JSON.parse(fbRes);
-            let Resuid = JSON.parse(fbRes).id;
+            let Resuid   = JSON.parse(fbRes).id;
             db.usersocialauth.findAndCountAll({
                 where:{
                     uid:Resuid,
@@ -138,10 +139,8 @@ module.exports = {
                             console.log('userTokenData',JSON.parse(JSON.stringify(userTokenData)),userResponse);
                             let usertoken  =  JSON.parse(JSON.stringify(userTokenData))[0].token
                             let provider   =  true;
-                            var templet        =  "<div><h1>Welcome to Impact Club "+userResponse.first_name+" "+userResponse.last_name + " </h1><p>Get fit do good</p><img src='https://i.gifer.com/47in.gif' width='300' height='200'></div>"
 
                             req.headers.authorization = 'bearer '+ usertoken;
-                            sendmail.sendMail("Welcome "+userResponse.first_name,userResponse.email,"Hello","Welcome to Impact Club",templet);
                             next();
                         })
                     })
@@ -154,11 +153,11 @@ module.exports = {
     // calling Google graph api for singning user and creating new user share_api_user table in db
     authenticatingUserGoogle(token,req,res,next){
         const tokenheader = req.headers.authorization;
-        var parts = tokenheader.split(' ');
-        const options = {
-            method: 'GET',
-            uri: 'https://www.googleapis.com/plus/v1/people/me?access_token=' + token,
-        };
+        var parts         = tokenheader.split(' ');
+        const options     = {
+                                method: 'GET',
+                                uri: 'https://www.googleapis.com/plus/v1/people/me?access_token=' + token,
+                            };
         request(options)
         .then(GoogleRes => {
             var tokengen    =  randomToken(25);
